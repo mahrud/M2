1f510b17a8632315fc793c59c72f212e20d922cc
Mike Stillman <mikestillman1@gmail.com>
2022-10-10 15:08:22 -0400

    (MIKE) refactor to separate MonomialOrder and MonomialEncoder

--- M2/Macaulay2/e/unit-tests/MonomialOrderTest.cpp
+++ M2/Macaulay2/e/unit-tests/MonomialOrderTest.cpp
@@ -6,26 +6,65 @@
 TEST(MonomialOrder, createFromArray)
 {
   std::vector<std::pair<MO::order_type, std::vector<int>>> array1 {
-    {MO::Lex, {4, 32}},
+    {MO::Lex, {3}},
     {MO::Weights, {0, 1, 2, 3, 4}},
-    {MO::GRevLexWeights, {4, 32, 1, 1, 1, 1}},
+    {MO::GRevLex, {4}},
+    {MO::GRevLexWeights, {1, 1, 1, 1, 2}},
     {MO::PositionUp, {}}
   };
-  NewAgeMonomialOrder mo1 {8, array1};
-
-  NewAgeMonomialOrder mo2 {8, {
-    {MO::Lex, {4, 32}},
+  NewAgeMonomialOrder mo1 {12, array1};
+  auto array2 = mo1.toMonomialType();
+  EXPECT_EQ(array1, array2);
+  std::cout << "--- display monomialType---" << std::endl;
+  std::cout << mo1.toMonomialType() << std::endl;
+  std::cout << "--- debug display ---" << std::endl;
+  mo1.debugDisplay(std::cout);
+  
+  NewAgeMonomialOrder mo2 {12, {
+    {MO::Lex, {3}},
     {MO::Weights, {0, 1, 2, 3, 4}},
-    {MO::GRevLexWeights, {4, 32, 1, 1, 1, 1}},
+    {MO::GRevLex, {4}},
+    {MO::GRevLexWeights, {1, 1, 1, 1, 2}},
     {MO::PositionUp, {}}
     }};
 
   std::cout << "mo2 = " << mo2 << std::endl;
+  mo2.debugDisplay(std::cout);
+
+
+  NewAgeMonomialOrder mo3 {9, {
+    {MO::Lex, {3}},
+    {MO::RevLex, {2}},
+    {MO::GroupRevLex, {4}},
+    {MO::PositionUp, {}}
+    }};
+
+  std::cout << "mo3 = " << mo3 << std::endl;
+  mo3.debugDisplay(std::cout);
+
 }
 
-// TEST(MonomialOrder, createFromMatrix)
-// {
-// }
+TEST(MonomialOrder, packing)
+{
+  std::vector<std::pair<MO::order_type, std::vector<int>>> array1 {
+    {MO::Packing, {4}},
+    {MO::Lex, {3}},
+    {MO::Weights, {0, 1, 2, 3, 4}},
+    {MO::Packing, {2}},
+    {MO::GRevLex, {4}},
+    {MO::GRevLexWeights, {1, 1, 1, 1, 2}},
+    {MO::PositionUp, {}}
+  };
+  NewAgeMonomialOrder mo1 {12, array1};
+  auto array2 = mo1.toMonomialType();
+  EXPECT_EQ(array1, array2);
+  std::cout << mo1.toMonomialType() << std::endl;
+  std::cout << "--- debug display ---" << std::endl;
+  mo1.debugDisplay(std::cout);
+
+  std::cout << "mo1 = " << mo1 << std::endl;
+  
+}
 
 // TEST(MonomialOrder, arrayFromMonomialOrder)
 // {

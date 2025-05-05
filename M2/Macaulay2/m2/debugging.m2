mahrud/M2

commit cfaad9bcc0d0b99892b6225fa010cdf1f6b0cbf9

Refactor contents of TestInput
Rather than storing the filename and line number separately, we store
a FilePosition containing both of them.  We update locate(TestInput)
to use this.

We also remove the redundant source file stamp from the code -- there's
no reason to also store the file location here.

Finally, we remove the NewFromMethod for constructing a TestInput from
a sequence.  There was really no reason to use this, as we only
construct a TestInput object once, inside TEST.

M2/Macaulay2/m2/testing.m2
@@ -3,28 +3,14 @@ needs "code.m2"
 needs "run.m2"
 
 -----------------------------------------------------------------------------
--- Local utilities
------------------------------------------------------------------------------
-
-sourceFileStamp = (filename, linenum) -> toString commentize(
-    pos := new FilePosition from (toAbsolutePath filename, linenum, 1);
-    toString pos, ": location of test code")
-
------------------------------------------------------------------------------
 -- TestInput
 -----------------------------------------------------------------------------
 TestInput = new SelfInitializingType of HashTable
-new TestInput from Sequence := (T, S) -> TestInput {
-    "filename" => S_0,
-    "line number" => S_1,
-    "code" => concatenate(sourceFileStamp(S_0, S_1), newline, S_2)}
 TestInput.synonym = "test input"
 
 code TestInput := T -> T#"code"
-locate TestInput := T -> new FilePosition from (T#"filename",
-    T#"line number" - depth net code T, 1,
-    T#"line number", 1,,)
 toString TestInput := toString @@ locate
+locate TestInput := T -> T#"location"
 net TestInput := lookup(net, Function)
 precedence TestInput := lookup(precedence, Function)
 editMethod TestInput := editMethod @@ locate
@@ -37,12 +23,21 @@ TEST = method(Options => {FileName => false})
 TEST List   := opts -> testlist   -> apply(testlist, test -> TEST(test, opts))
 TEST String := opts -> teststring -> (
     n := currentPackage#"test number";
-    currentPackage#"test inputs"#n = TestInput if opts.FileName then (
-        testCode := get teststring;
-        (minimizeFilename teststring, depth net testCode + 1, testCode)
-        ) else
-        (minimizeFilename currentFileName, currentRowNumber(), teststring);
-    currentPackage#"test number" = n + 1;)
+    if opts.FileName then (
+	filename := teststring;
+	teststring = get filename;
+	start := 1;
+	stop := depth net teststring)
+    else (
+	filename = currentFileName;
+	stop = currentRowNumber() - 1;
+	start = stop - depth net teststring);
+    currentPackage#"test inputs"#n = TestInput {
+	"location" => new FilePosition from {
+	    minimizeFilename filename,
+	    start, 1, stop, 80}, -- TODO: get actual final column
+	"code" => teststring};
+    currentPackage#"test number" += 1;)
 -- TODO: support test titles
 
 -----------------------------------------------------------------------------

commit 7355fc099ef60668894ec3238ed91188c2b14875

updated check and runFile
this partially reverts cfaad9bc, which broke a file stamp set in testing.m2 and regex matched in run.m2

M2/Macaulay2/m2/debugging.m2
@@ -233,8 +233,14 @@ generateAssertions List := y -> (
 	       else lin
 	       )))^-1
 
+-----------------------------------------------------------------------------
+-- FilePosition and currentPosition
+-----------------------------------------------------------------------------
+
 -- FilePosition = new Type of BasicList -- defined in d
 FilePosition.synonym = "file position"
+
+-- TODO: add FilePosition(String, ZZ, ZZ) and FilePosition(String)
 toExternalString FilePosition :=
 toString FilePosition :=
 net FilePosition := p -> concatenate(
@@ -243,7 +249,15 @@ net FilePosition := p -> concatenate(
     if #p>3 then ("-",toString p#3,":",toString p#4),
 --    if #p>5 then (" (",toString p#5,":",toString p#6,")")
     )
+
+String | FilePosition := (s, p) -> s | toString p
+FilePosition | String := (p, s) -> toString p | s
+
 currentPosition = () -> new FilePosition from { currentFileName, currentRowNumber(), currentColumnNumber() }
+
+-----------------------------------------------------------------------------
+-- locate
+-----------------------------------------------------------------------------
 
 locate' = locate -- defined in d/actors4.d
 locate = method(Dispatch => Thing, TypicalValue => FilePosition)

M2/Macaulay2/m2/run.m2
@@ -15,10 +15,24 @@ ulimit := utest "-c unlimited" | utest "-t 700" | utest "-m 850000" | utest "-s 
 M2statusRegexp := "^--status:"
 statusLines := file -> select(lines file, s -> match(M2statusRegexp,s))
 
+testLocation := inf -> (
+    infcontents := get inf;
+    -- TODO: is this feature ever used?
+    scan(statusLines infcontents, printerr);
+    m := regex("^-- test source: ([^\n]+)", infcontents);
+    if m =!= null then substring(m#1, infcontents)
+    else toString new FilePosition from (inf, 0, 1))
+
 M2errorRegexp := "^[^:\n]+:[0-9]+:[0-9]+:(\\([0-9]+\\)):\\[[0-9]+\\]: "
 aftermatch := (pat,str) -> (
     m := regex(pat,str);
     if m === null then "" else substring(m#0#0,str))
+
+logLocation := tmpf -> concatenate(
+    toString new FilePosition from (tmpf, 0, 1),
+    ":(" | loadDepth | ")",
+    ":[" | recursionDepth() | "]",
+    ": ", aftermatch(M2errorRegexp, get tmpf))
 
 describeReturnCode := r -> (
     if r % 256 == 0 then "exited with status code " | toString (r // 256)
@@ -128,7 +142,6 @@ runFile = (inf, inputhash, outf, tmpf, pkg, announcechange, usermode, examplefil
      cmd = cmd | readmode(SetInputFile,   "<" | format inf);
      cmd = cmd | readmode(SetOutputFile,  ">>" | format toAbsolutePath tmpf);
      cmd = cmd | readmode(SetCaptureErr,  "2>&1");
-     if debugLevel > 0 then stderr << endl << cmd << endl;
      for fn in examplefiles do copyFile(fn,rundir | baseFilename fn);
      r := run cmd;
      if r == 0 then (
@@ -136,21 +149,18 @@ runFile = (inf, inputhash, outf, tmpf, pkg, announcechange, usermode, examplefil
 	  moveFile(tmpf,outf);
 	  return true;
 	  );
-     if debugLevel == 0 then stderr << endl;
-     stderr << cmd << endl;
-     stderr << (new FilePosition from (tmpf, 0, 1)) << ":(" << loadDepth << "):[" << recursionDepth() << "]: (output file) error: Macaulay2 " << describeReturnCode r << endl;
-     stderr << aftermatch(M2errorRegexp,get tmpf);
-     infcontents := get inf;
-     stderr << (new FilePosition from (inf, 0, 1)) << ": (input file)" << endl;
-     scan(statusLines infcontents, x -> stderr << x << endl);
-     m := regex("^--([^\n]+): location of test code", infcontents);
-     if m =!= null then stderr << (
-	 substring(m#1, infcontents)) << ": (location of test code)" << endl;
-     if # findFiles rundir == 1
-     then removeDirectory rundir
+     -- print debugging info
+     printerr "running failed, too!";
+     printerr("error log:  " | logLocation tmpf);
+     printerr("test input: " | testLocation inf);
+     if debugLevel > 0 then
+     stderr << commentize "full command:" << endl << cmd << endl;
+     stderr << pad("*** error: M2 " | describeReturnCode r, printWidth - 32) << flush;
+     -- cleanup run directory
+     if # findFiles rundir == 1 then removeDirectory rundir
      else stderr << rundir << ": error: files remain in temporary run directory after program exits abnormally" << endl;
-     stderr << "M2: *** Error " << (if r<256 then r else r//256) << endl;
-     if r == 2 then error "interrupted";
+     -- detect user interrupts
+     if r // 256 == 2 then error "interrupted";
      numExampleErrors += 1;
      return false;
      )

M2/Macaulay2/m2/testing.m2
@@ -27,7 +27,7 @@ addTest(String, FilePosition) := (str, loc) -> (
     n := #currentPackage#"test inputs";
     currentPackage#"test inputs"#n = TestInput {
 	"location" => loc,
-	"code" => str})
+	"code" => concatenate("-- test source: ", toString loc, newline, str)})
 -- the following is not called by TEST, but called directly when we want to
 -- add a test from a file (used by loadTestDir)
 addTest String := filename -> addTest(get filename,
@@ -115,7 +115,10 @@ check(List, Package) := opts -> (L, pkg) -> (
 	if opts.Verbose then apply(errorList, (k, errfile) -> (
 		stderr << locate inputs#k << " error:" << endl;
 		printerr getErrors(outfile errfile)));
-	error("test(s) #", demark(", ", toString \ first \ errorList), " of package ", toString pkg, " failed.")))
+	printerr("Summary: ", toString(#errorList), " test(s) failed in package ", pkg#"pkgname", ":");
+	printerr net TABLE apply(first \ errorList, i -> { "Test #"|i|".", toString locate tests_i pkg });
+	error("repeat failed tests with:", newline,
+	    "  check({", demark(", ", toString \ first \ errorList), "}, ", format pkg#"pkgname", ")")))
 
 checkAllPackages = () -> (
     tmp := argumentMode;

M2/Macaulay2/packages/Macaulay2Doc/ov_debugging.m2
@@ -358,7 +358,6 @@ document {
      SeeAlso => { "recursionLimit" }
      }
 
-
 document {
      Key => FilePosition,
      Headline => "the class of all file positions",
@@ -369,6 +368,10 @@ document {
      A single pair is a position, two form a range. The last pair is the central point of interest in that range."
      }
 
+undocumented {
+    (symbol |, FilePosition, String),
+    (symbol |, String, FilePosition),
+}
 
 document {
      Key => uncurry,

M2/Macaulay2/tests/normal/testing.m2
@@ -8,7 +8,9 @@ check "TestPackage"
 pkgtest = tests(0, "TestPackage")
 assert instance(pkgtest, TestInput)
 assert Equation(toSequence locate pkgtest, (testpkg, 3, 5, 3, 32, 3, 5))
-assert Equation(toString pkgtest, "assert Equation(1 + 1, 2)")
+assert Equation(toString pkgtest, concatenate(
+	"-- test source: ", toString locate pkgtest, newline,
+	"assert Equation(1 + 1, 2)"))
 assert Equation(net pkgtest, "TestInput[" | testpkg | ":3:5-3:32]")
 beginDocumentation()
 expectedCode = DIV{